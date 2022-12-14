fa187c76b4e796186a1a9f20e8c9f046c390ac06
cat-boop <sushenkoroman2003@gmail.com>
2022-12-14 12:30:24 +0300

    add db

--- backend/src/main/java/com/rsushe/weblab4/entity/User.java
+++ backend/src/main/java/com/rsushe/weblab4/entity/User.java
@@ -1,13 +1,18 @@
 package com.rsushe.weblab4.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "`User`")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -17,9 +22,14 @@ public class User {
     @GeneratedValue
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
     private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
--- backend/src/main/java/com/rsushe/weblab4/repository/UserRepository.java
+++ backend/src/main/java/com/rsushe/weblab4/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.rsushe.weblab4.repository;
 
 import com.rsushe.weblab4.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findFirstByUsername(String username);
 }
