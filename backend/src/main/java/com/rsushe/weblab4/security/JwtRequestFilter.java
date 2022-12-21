3e48a556ba775877b800043fc2742996a4994083
cat-boop <sushenkoroman2003@gmail.com>
2022-12-22 00:39:19 +0300

    optimize imports

--- backend/src/main/java/com/rsushe/weblab4/security/JwtRequestFilter.java
+++ backend/src/main/java/com/rsushe/weblab4/security/JwtRequestFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
